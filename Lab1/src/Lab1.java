a7bb093cd8183695b5de297f3d6fbc08bfc1b4ca
Britov Sergey <44268267+s0s0chka@users.noreply.github.com>
2022-10-01 19:35:09 +0300

    Update Lab1.java

--- Lab1/src/Lab1.java
+++ Lab1/src/Lab1.java
@@ -1,20 +1,19 @@
 public class Lab1 {
 
-    public static void printArray(double[][] array){
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array[0].length; j++){
+    public static void printArray(double[][] array) {
+        for(int i = 0; i < array.length; i++) {
+            for(int j = 0; j < array[0].length; j++) {
                 System.out.printf("%8.4f ", array[i][j]);
             }
             System.out.println();
         }
     }
 
-    public static double generateNumber(short condition, float x){
+    public static double generateNumber(short condition, float x) {
         double result;
-
-        if (condition == 6){
+        if (condition == 6) {
             result = Math.cbrt(Math.tan(Math.cos(x)));
-        } else if(condition == 8 || condition == 12 || condition == 14 || condition == 20){
+        } else if(condition == 8 || condition == 12 || condition == 14 || condition == 20) {
             result = Math.log(Math.pow(Math.tan(Math.cos(Math.asin((x + 0.5) / 25))), 2));
         } else {
             result = Math.pow(Math.E, Math.pow((2/3) / (Math.pow(Math.pow(Math.E, x), Math.pow(2 - x, 3) / 2 - 1)), 3));
@@ -42,7 +41,6 @@ public class Lab1 {
 
         //Task 3
         double p[][] = new double[9][11];
-
         for (int i = 0; i < p.length; i++){
             for (int j = 0; j < p[0].length; j++){
                 p[i][j] = generateNumber(f[i], x[j]);
